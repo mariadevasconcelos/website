1013e80a69a69f77e806e8425d15b4f20813f8b9
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-10-28 23:55:08 +0000

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,27 @@
+/* * */
+
+import { MantineProvider } from '@mantine/core';
+import { Analytics } from '@vercel/analytics/react';
+
+/* * */
+
+import '@mantine/core/styles.css';
+
+import './reset.css';
+
+/* * */
+
 export const metadata = {
 	title: 'react-pdf sample page',
 };
 
-import { MantineProvider } from '@mantine/core';
-
-import './reset.css';
-import '@mantine/core/styles.css';
+/* * */
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en-US">
 			<body>
+				<Analytics />
 				<MantineProvider>
 					{children}
 				</MantineProvider>
